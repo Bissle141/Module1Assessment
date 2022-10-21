a8e03ce0af926f63adc4443bd330849b30cd2615
Alyssa Black <alyssa.black141@gmail.com>
2022-10-21 10:44:37 -0600

    finished and commented arrays

--- arrays.js
+++ arrays.js
@@ -5,10 +5,15 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0); //creats the new array and sets it equal to the sliced values including and proceeding the 0 index of favColors
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push('forest green'); //appends the colorCopy array with my favorite color, forest green. It's better than just green and technically could be considered a diff color!!
+// console.log(colorCopy); //Checks that the array was set properly
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,6 +25,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+let middleNums = numbers.slice(1,4) //creats the new var, sets it equal to the 1-3 indexes of numbers array using the slice method
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +41,16 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+//create loop, sets iterator to 0 for zero indexing, condition makes it so it will run so long as iterator is less than length (uses .lenght method), increment by 1
+//within loop check is indexed num is > 100, if so push 'big' to answers array
+//within loop if indexed num is < 100 or equal to 100, push 'small' to answers array
+
+let answers = []; // creates an empty array
+for(let i = 0; i < bigOrSmallArray.length; i++){
+  if (bigOrSmallArray[i] > 100){
+    answers.push('big');
+  } else {
+    answers.push('small');
+  }
+}
+// console.log(answers); // to check results
